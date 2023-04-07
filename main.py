6ea01b6d05a66c67a3460e3b367ff449e047669a
WiktorWysocki <wiktorwysockig5@gmail.com>
2023-04-07 08:58:13 +0200

    added tensorflow

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import numpy, random, os
+import numpy
+import random
+import tensorflow as tf
+
 lr = 1 #learning rate
 bias = 1 #value of bias
 weights = [random.random(),random.random(),random.random()] #weights generated in a list (3 weights in total for 2 neurons and the bias)
